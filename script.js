96d09c5233b9d5669d536d0f56e9be69a84c8792
karinemso <karinemsoliveira@gmail.com>
2024-09-16 22:57:18 -0300

    css adjusts

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ const initial = document.querySelector('.initial')
 const about = document.querySelector('.about')
 const btnHowItWorks = document.querySelector('#howItWorks')
 const btnReturn = document.querySelector('#return')
+const byBlaBlaLab = document.querySelector('#byBlaBlaLab')
 
 
 function goTo(from){
     const currentPage = from == 'initial'? initial : about
     const nextPage = from == 'initial'? about : initial
 
+    
     currentPage.style.opacity = '0'
     console.log('oi')
     setTimeout(() => {
-
+        
+        if(from == 'initial'){
+            byBlaBlaLab.style.display = 'block'
+        } else {
+            byBlaBlaLab.style.display = 'none'
+    
+        }
          currentPage.style.display = 'none'
          nextPage.style.display = 'flex'
-        },300)
 
+         
+        },300)
+        
         setTimeout(() => {
             
+            if(from == 'initial'){
+               byBlaBlaLab.style.opacity = '1'
+           } else {
+               byBlaBlaLab.style.opacity = '0'
+       
+           }
             nextPage.style.opacity = '1'
         }, 400);
 
